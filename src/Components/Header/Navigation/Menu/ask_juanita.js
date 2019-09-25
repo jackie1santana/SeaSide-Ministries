e8353f15b831c52bf2ea9d07a85d0f27b96e6ce3
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-09-24 20:52:55 -0500

    fixed footer and added form

--- src/Components/Header/Navigation/Menu/ask_juanita.js
+++ src/Components/Header/Navigation/Menu/ask_juanita.js
@@ -4,6 +4,17 @@ export default function ask_juanita() {
     return (
         <div>
             <h1>Ask Pastor Juanita</h1>
+            <form>
+                <label>
+                    <input type="text" name="name" value="Name" />
+                    <br/>
+                    <input type="text" name="name" value="Email" />
+                    <br/>
+                    <input type="text" name="name" value="Leave a Comment .." />
+                </label>
+                <br/>
+                    <input type="submit" value="Send Message" />
+            </form>
         </div>
     )
 }
